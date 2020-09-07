547a682a05ea0201c3dda03f88fa5142af1f8073
kodfactory <52972405+kodfactory@users.noreply.github.com>
2020-09-07 19:21:30 +0530

    Add files via upload

---
+++ TupleExample.py
@@ -0,0 +1,18 @@
+listA = [1,2,3,4]
+
+tupleA = (1,2,3,4)
+
+# 1. Syntax: List has [] while tuple has ()
+# 2. It's a immutable datatype, i.e we cannot change
+
+# tupleA.append(10)
+
+# tupleA.remove(3)
+
+for i in tupleA:
+    print(i)
+
+# print(type(tupleA))
+# print(tupleA[0:2])
+
+
